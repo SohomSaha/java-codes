0ac20cba9f3c1f110d6c707a2536781b0f689ffa
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2021-12-26 09:38:13 +0530

    Add files via upload
    This is my first code in github

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class add
+{
+public static void main(String[] args) {
+       
+    System.out.println("Enter two numbers "); 
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int s=a+b;
+       System.out.println("sum of two numbers is "+s); 
+    }
+}
