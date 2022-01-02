cbc62b36a8d469c0c422df6f18370f5ee744bd9e
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-01-02 10:47:23 +0530

    Time: 11 ms (17.34%), Space: 42.1 MB (15.34%) - LeetHub

---
+++ roman-to-integer/roman-to-integer.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int romanToInt(String s) {s=s+" ";int sum=0;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            if(s.charAt(i)=='I')
+            {
+                if(s.charAt(i+1)=='V')
+                {sum+=4;i++;}
+                else if(s.charAt(i+1)=='X')
+                {sum+=9;i++;}
+                else
+                    sum+=1;
+            }
+             else if(s.charAt(i)=='X')  
+             {
+             if(s.charAt(i+1)=='L') 
+                 {sum+=40;i++;}
+                else if(s.charAt(i+1)=='C')
+                {sum+=90;i++;}
+                 else
+                     sum+=10;
+             }
+              else if(s.charAt(i)=='C')
+              {
+                 if(s.charAt(i+1)=='D')
+                {sum+=400;i++;}
+                else if(s.charAt(i+1)=='M')
+                {sum+=900;i++;}
+                else
+                    sum+=100;
+              }
+              else if(s.charAt(i)=='L') 
+                 sum+=50;
+              else if(s.charAt(i)=='V')
+                sum+=5;
+                else if(s.charAt(i)=='D')
+                sum+=500;
+                else if(s.charAt(i)=='M')
+                    sum+=1000;
+        }
+  return sum;  }
+}
