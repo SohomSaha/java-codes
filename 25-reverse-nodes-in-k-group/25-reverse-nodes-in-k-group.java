190f64dce056867135fdee1769375a3deb52cf10
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-03-16 10:20:01 +0530

    Update 25-reverse-nodes-in-k-group.java

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -14,15 +14,15 @@ class Solution {
         dummy.next=head;
         ListNode cur=dummy,nex=dummy,pre=dummy;
         int count=0;
-        while(cur.next!=null)
+        while(cur.next!=null)//length of linkedlist
         {
             count++;cur=cur.next;
         }
-        while(count>=k)
+        while(count>=k)//count to take out the k groups to reverse nodes
         {
             cur=pre.next;
             nex=cur.next;
-            for(int i=1;i<k;i++)
+            for(int i=1;i<k;i++)//reversing (k-1)times
             {
                 cur.next=nex.next;
                 nex.next=pre.next;
