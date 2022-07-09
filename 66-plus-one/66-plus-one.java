d89ad1d15cf983d76f8048c468b87a94838a9333
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-07-09 12:05:03 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (61.56%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,39 +1,24 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n=digits.length;
-        int i=0,e=0;
-        if(digits[n-1]==9)
-        {
-            digits[n-1]=0;
-            i=index(digits,n-2);
-        
-            if(i!=-1)
-             digits[i]+=1;
-            else
-            e=1;  
-        }
-        else
-            digits[n-1]+=1;
-        if(i!=-1)    
+        int plus=1;
+       for(int i=digits.length-1;i>=0;i--)
+       {
+           if(digits[i]+plus>9)
+               digits[i]=0;
+           else
+           {
+               digits[i]+=1;
+               plus=0;
+               break;
+           }
+       }
+        if(plus!=1)
             return digits;
         else
         {
-            int []res=new int[n+1];
-            res[0]=e;
-            for(int j=1;j<=n;j++)
-                res[j]=digits[j-1];
+            int []res=new int[digits.length+1];
+            res[0]=1;
             return res;
-        } 
-    }
-    public int index(int[] digits,int i)
-    {
-           if(i<0)
-                return -1; 
-        if(digits[i]==9)
-        {
-            digits[i]=0;
-            return index(digits,i-1);
         }
-        return i;  
-    }    
+    }
 }
