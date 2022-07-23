ae9c0080a81d88330e07d81dc510528afd968db1
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-07-23 12:24:51 +0530

    Time: 43 ms (46.65%), Space: 79.6 MB (61.74%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.java
@@ -5,11 +5,8 @@ class Solution {
         int rmv=0;
         for(int i=0;i<nums.length;i++)
         {
-                if(h.containsKey(nums[i]))
-                {
-                    if(Math.abs(i-h.get(nums[i]))<=k)
+                if(h.containsKey(nums[i])&& Math.abs(i-h.get(nums[i]))<=k)
                     return true;
-                }
                 h.put(nums[i],i);
         }
         return false;
