a1944014845ca2e47fa7411bf04c872c246597e2
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-07-23 11:20:43 +0530

    Time: 16 ms (38.74%), Space: 42.8 MB (66.85%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -3,7 +3,7 @@ class Solution {
         HashMap<Character,Character>map=new HashMap<>();
         for(int i=0;i<s.length();i++)
         {
-             if(!map.containsKey(s.charAt(i))&& map.containsValue(t.charAt(i))||(map.containsKey(s.charAt(i))&&!map.containsValue(t.charAt(i))))
+             if(!map.containsKey(s.charAt(i))&& map.containsValue(t.charAt(i)))
                  return false;
             else if(!map.containsKey(s.charAt(i)))
                 map.put(s.charAt(i),t.charAt(i));
