6b4911ab926879538ca575cd7d2a75b1336c58d4
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-07-24 17:34:06 +0530

    Time: 5 ms (75.47%), Space: 44.7 MB (41.02%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,23 +1,15 @@
-import java.util.*;
+import java.util.Arrays;
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length())
             return false;
-        Hashtable<Character,Integer> h=new Hashtable<>();
-        for(int i=0;i<s.length();i++)
-        {
-            if(!h.containsKey(s.charAt(i)))
-                h.put(s.charAt(i),1);
-               else
-               h.put(s.charAt(i),h.get(s.charAt(i))+1);
-        }
-         for(int i=0;i<t.length();i++)
-               {
-                   if(h.containsKey(t.charAt(i))&&h.get(t.charAt(i))!=0)
-                       h.put(t.charAt(i),h.get(t.charAt(i))-1);
-             else
-                return false; 
-               }
-               return true;
+      char arr1[]=s.toCharArray();
+	char arr2[]=t.toCharArray();
+	Arrays.sort(arr2);
+	Arrays.sort(arr1);
+        if(Arrays.equals(arr1,arr2))
+            return true;
+        return false;
+            
     }
 }
