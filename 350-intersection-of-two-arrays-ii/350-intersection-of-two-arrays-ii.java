a8a027382802210f2b5de9347c5ea54fea0d1a68
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-07-25 13:58:21 +0530

    Time: 3 ms (85.65%), Space: 44.7 MB (9.85%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -2,19 +2,20 @@ import java.util.*;
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         List<Integer>list=new ArrayList<>();
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int p1=0,p2=0,j=0;
-        while(p1!=nums1.length&&p2!=nums2.length)
+       HashMap<Integer,Integer>map=new HashMap<>();
+        int j=0;
+        for(int i=0;i<nums1.length;i++)
         {
-            if(nums1[p1]<nums2[p2])
-                p1++;
-            else if(nums1[p1]>nums2[p2])
-                p2++;
+           if(!map.containsKey(nums1[i]))
+            map.put(nums1[i],1);
             else
-            {
-                list.add(nums1[p1]);
-                p1++;p2++;
+                map.put(nums1[i],map.get(nums1[i])+1);
+        }
+        for(int i=0;i<nums2.length;i++)
+        {
+            if(map.containsKey(nums2[i])&&map.get(nums2[i])!=0)
+            {list.add(nums2[i]);
+             map.put(nums2[i],map.get(nums2[i])-1);
             }
         }
         int []res=new int[list.size()];
