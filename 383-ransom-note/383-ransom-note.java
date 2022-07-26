f7eaa4dab960a557b4b30aa3e1e1b28d1f99cd0c
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-07-26 09:55:45 +0530

    Time: 31 ms (26.26%), Space: 52 MB (28.40%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -4,17 +4,19 @@ class Solution {
         HashMap<Character,Integer>map=new HashMap<>();
         for(int i=0;i<magazine.length();i++)
         {
-            if(!map.containsKey(magazine.charAt(i)))
-               map.put(magazine.charAt(i),1);
+            char c=magazine.charAt(i);
+            if(!map.containsKey(c))
+               map.put(c,1);
             else
-               map.put(magazine.charAt(i),map.get(magazine.charAt(i))+1);
+               map.put(c,map.get(c)+1);
         }
         for(int i=0;i<ransomNote.length();i++)
         {
-            if(!map.containsKey(ransomNote.charAt(i))||map.get(ransomNote.charAt(i))==0)
+            char c=ransomNote.charAt(i);
+            if(!map.containsKey(c)||map.get(c)==0)
                 return false;
             else
-                map.put(ransomNote.charAt(i),map.get(ransomNote.charAt(i))-1);    
+                map.put(c,map.get(c)-1);    
         }
                return true;
     }
