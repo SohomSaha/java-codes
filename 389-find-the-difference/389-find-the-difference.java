1e6b0ad38de1db5cc8c0ea2547a3143f3a97579b
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-07-26 13:40:33 +0530

    Time: 3 ms (55.20%), Space: 42.2 MB (57.05%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i=0;i<t.length();i++)
         {
             if(i!=s.length())
-                sum1=sum1+(s.charAt(i)-'a');
-            sum2=sum2+(t.charAt(i)-'a');
+                sum1=sum1+(s.charAt(i));
+            sum2=sum2+(t.charAt(i));
         }
-        return (char)((sum2-sum1)+'a');
+        return (char)(sum2-sum1);
     }
 }
