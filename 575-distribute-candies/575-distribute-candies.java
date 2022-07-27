0cbb74ec419f7debd6bfd1173a10d74e72bd4168
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-07-27 15:43:07 +0530

    Time: 59 ms (53.54%), Space: 104.7 MB (60.72%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.java
+++ 575-distribute-candies/575-distribute-candies.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public int distributeCandies(int[] candyType) {
-        HashSet<Integer>set=new HashSet<>();
+        HashSet<Integer>set=new HashSet<>(candyType.length);
         for(int i: candyType)
             set.add(i);
         return Math.min(set.size(),candyType.length/2);
