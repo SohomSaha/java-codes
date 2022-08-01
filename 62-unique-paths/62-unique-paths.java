46cd6d902ec59b82e86e6e1aea4d7b779449658a
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-08-01 15:41:24 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (27.16%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if(m==1||n==1)
+            return 1;
+        m--;
+        n--;
+        if(m<n)
+        {
+            m=m+n;
+            n=m-n;
+            m=m-n;
+        }
+        long res=1;
+        int j=1;
+        for(int i=m+1;i<=m+n;i++)
+            res=res*i/j++;
+        return(int)res;
+    }
+}
