dcff8172299dab20082d27d97a986fd6e7006996
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-08-08 09:20:46 +0530

    Time: 3 ms (98.97%), Space: 44.6 MB (37.33%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int len = 0;
+        for (int i = 1; i < nums.length; i++) {
+            int pos = binarySearch(dp,len,nums[i]);
+            if (nums[i] < dp[pos]) dp[pos] = nums[i];
+            if (pos > len) {
+                len = pos;
+                dp[len] = nums[i];
+            }
+        }
+        return len+1;
+    }
+    private int binarySearch(int[] dp, int len, int val) {
+        int left = 0;
+        int right = len;
+        while(left+1 < right) {
+            int mid = left + (right-left)/2;
+            if (dp[mid] == val) {
+                return mid;
+            } else {
+                if (dp[mid] < val) {
+                    left = mid;
+                } else {
+                    right = mid;
+                }
+            }
+        }
+        if (dp[right] < val) return len+1;
+        else if (dp[left] >= val) return left;
+        else return right;
+    }
+}
