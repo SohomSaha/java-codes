4c1744dedbe6c83e342fa1f0b4551e772c042245
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-08-09 08:57:50 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (50.80%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,10 @@
+class Solution {
+    int dp[]=new int[46];
+    public int climbStairs(int n) {
+        if(n<=1)
+            return 1;
+        if(dp[n]!=0)
+            return dp[n];
+        return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+    }
+}
