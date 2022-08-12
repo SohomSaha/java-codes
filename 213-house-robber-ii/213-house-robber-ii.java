1fe931e7ad1c67c1b66a7a000042cb9d8ba4fc1a
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-08-12 16:43:30 +0530

    Time: 0 ms (100.00%), Space: 41 MB (80.89%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -2,22 +2,21 @@ class Solution {
     public int rob(int[] nums) {
         if(nums.length==1)
             return nums[0];
-        ArrayList<Integer>list=new ArrayList<>();
+       /* ArrayList<Integer>list=new ArrayList<>();
         for(int i:nums)
             list.add(i);
-        list.remove(0);
-         int pick=robber(list,list.size()-1);
-        list.add(0,nums[0]);
-        list.remove(nums.length-1);
-        int nopick=robber(list,list.size()-1);
+        list.remove(0);*/
+         int pick=robber(nums,nums.length-2,0);
+        /*list.add(0,nums[0]);
+        list.remove(nums.length-1);*/
+        int nopick=robber(nums,nums.length-1,1);
         return Math.max(pick,nopick);
-        
     }
-     public int robber(ArrayList<Integer>nums,int ind)
+     public int robber(int[] nums,int ind,int t)
     {
-       int prev2=0,prev1=nums.get(0),curr=0;
-        for(int i=1;i<=ind;i++){
-           int pick=nums.get(i)+prev2; 
+       int prev2=0,prev1=nums[t],curr=0;
+        for(int i=1+t;i<=ind;i++){
+           int pick=nums[i]+prev2; 
         int nopick=0+prev1;
             curr=Math.max(pick,nopick);
             prev2=prev1;
