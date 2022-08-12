e6df8bbce089f037d913b4ecc2e7ec691d408584
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-08-12 16:46:40 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (82.09%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -8,9 +8,7 @@ class Solution {
     {
        int prev2=0,prev1=nums[t],curr=0;
         for(int i=1+t;i<=ind;i++){
-           int pick=nums[i]+prev2; 
-        int nopick=0+prev1;
-            curr=Math.max(pick,nopick);
+            curr=Math.max(nums[i]+prev2,0+prev1);
             prev2=prev1;
             prev1=curr;
         }
