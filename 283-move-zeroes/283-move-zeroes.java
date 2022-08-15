1c0d0d8cf16b6bd26fd10e2ba6bbe0b40f9f3f58
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-08-15 18:49:21 +0530

    Time: 2 ms (82.89%), Space: 55.2 MB (20.68%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,22 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int ball=0,check=0;
+        int ball=0;
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==0)
-            {
                 ball++;
-            }
             else if(ball!=0)
             {
-                swap(ball,i,nums);
-                check=0;
+                nums[i-ball]=nums[i];
+                nums[i]=0;
             }
         }
-    }
-    public void swap(int ball,int index,int []nums)
-    {
-        nums[index-ball]=nums[index];
-        nums[index]=0;
     }
 }
