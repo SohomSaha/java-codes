040f9545b28c03228dec3bca1baff30eeb1b2f95
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-08-15 10:03:32 +0530

    Time: 51 ms (21.38%), Space: 54.8 MB (11.05%) - LeetHub

--- 929-unique-email-addresses/929-unique-email-addresses.java
+++ 929-unique-email-addresses/929-unique-email-addresses.java
@@ -1,31 +1,20 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
-        HashMap<String,HashSet<String>>map=new HashMap<>();
-        int len=0;
+        HashSet<String>set=new HashSet<String>(emails.length);
         for(String s:emails)
-        {
-            int index=s.indexOf('@');
-            String local=filter(s.substring(0,index));
-            String domain=s.substring(index+1,s.length());
-            if(map.containsKey(domain))
-             map.get(domain).add(local); 
-           else 
-            map.put(domain,new HashSet<String>(Arrays.asList(local)));
-        }
-        for(HashSet<String> set:map.values())
-            len+=set.size();
-        return len;      
+            set.add(filter(s));
+        return set.size();
     }
-    public String filter (String s)
+    public String filter(String s)
     {
         String res="";
         for(char c:s.toCharArray())
         {
-            if(c=='+')
+            if(c=='+'||c=='@')
                 break;
-            else if(c!='.')
+            if(c!='.')
                 res=res+c;
         }
-        return res;
+        return res+s.substring(s.indexOf('@'),s.length());
     }
 }
