93bebb27b7d1a1c29a6efacf1f4cea42b74ec7bd
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-08-23 12:42:54 +0530

    Time: 11 ms (60.86%), Space: 56 MB (7.19%) - LeetHub

--- 817-linked-list-components/817-linked-list-components.java
+++ 817-linked-list-components/817-linked-list-components.java
@@ -13,18 +13,19 @@ class Solution {
     public int numComponents(ListNode head, int[] nums) {
         int c=0,f=0;
         ListNode prev=null;
-        ArrayList<Integer>list=new ArrayList<Integer>();
+        HashSet<Integer>set=new HashSet<Integer>();
         for(int i:nums)
-            list.add(i);
-        while(head!=null)
+            set.add(i);
+        while(head!=null&&set.size()!=0)
         {
-            if(list.contains(head.val))
+            if(set.contains(head.val))
             {
                 if(f==0)
                 {prev=head;c++;f++;}
                 else if(prev.next.val!=head.val)
                     c++;
                     prev=head;
+                set.remove(head.val);
             }
             head=head.next;
         }
