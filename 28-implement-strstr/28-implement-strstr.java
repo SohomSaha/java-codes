5cbd3af1a5c21d587941ba122b2769532812f697
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-08-31 17:21:55 +0530

    Time: 1 ms (64.52%), Space: 42.2 MB (38.89%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,5 +1,16 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-          return haystack.indexOf(needle);
+        for(int i=0;i<haystack.length();i++)
+        {
+            if(haystack.charAt(i)==needle.charAt(0))
+            {
+                if(haystack.length()-i>=needle.length())
+                {
+                if(needle.equals(haystack.substring(i,i+needle.length())))
+                    return i;
+                }
+            }
+        }
+        return -1;
     }
 }
