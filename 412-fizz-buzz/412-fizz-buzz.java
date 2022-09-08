e2a5562b239b86180238fe2b2087fddd15fe9e7a
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-09-08 06:31:40 +0530

    Time: 2 ms (75.41%), Space: 48 MB (76.88%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        String[]res=new String[n];
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3==0&&i%5==0)
+                res[i-1]="FizzBuzz";
+            else if(i%3==0)
+                res[i-1]="Fizz";
+            else if(i%5==0)
+                res[i-1]="Buzz";
+            else
+                res[i-1]=Integer.toString(i);    
+        }
+        return Arrays.asList(res);
+    }
+}
