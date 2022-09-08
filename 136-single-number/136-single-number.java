c3f1492a555afd3257d699792fae2c2fe3d85014
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-09-08 06:22:53 +0530

    Time: 2 ms (65.82%), Space: 50.2 MB (75.57%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,15 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-       HashSet<Integer>set=new HashSet<Integer>();
-        for(int i:nums)
-        {
-            if(set.contains(i))
-                set.remove(i);
-            else
-                set.add(i);
-        }
-        for(int i:set)
-            return i;
-        return 0;
+      int ans=0;
+        for(int i:nums )
+            ans^=i;
+        return ans;
     }
 }
