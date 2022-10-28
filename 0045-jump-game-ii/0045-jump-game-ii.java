979cb40aac9531c8fa2bcd62393c355d0362f39f
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-10-28 09:17:20 +0530

    Time: 166 ms (14.87%), Space: 43.5 MB (84.41%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,16 +1,16 @@
 class Solution {
     public int jump(int[] nums) {
-        int[]dp=new int[nums.length];
-        Arrays.fill(dp,-1);
+        Integer[]dp=new Integer[nums.length];
+        
         return nextjump(nums,0,dp);
     }
-    public int nextjump(int[]nums,int ind,int[]dp)
+    public int nextjump(int[]nums,int ind,Integer[]dp)
     {
         if(ind>=nums.length-1)
             return 0;
         if(nums[ind]==0)
             return (int)Math.pow(10,7);
-        if(dp[ind]!=-1)
+        if(dp[ind]!=null)
             return dp[ind];
         int min=Integer.MAX_VALUE;
         for(int i=1;i<=nums[ind];i++)
