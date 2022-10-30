239cddd9739e6770fdb91ae1d0a9cb0ef86ed3b5
Sohom Saha <93650703+SohomSaha@users.noreply.github.com>
2022-10-30 17:08:26 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (50.00%) - LeetHub

--- 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.java
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.java
@@ -13,7 +13,7 @@ class Solution {
         int s1=sum(n);
         if(s1<=target)
             return 0;
-        long temp=n;
+       /* long temp=n;
         int count=0;
         while(temp!=0)
         {
@@ -26,25 +26,27 @@ class Solution {
         {
             arr[i]+=n%10;
             n/=10;
-        }
+        }*/
         //System.out.println(Arrays.toString(arr));
-        long ans=0;
+        long ans=0,i=0;
         long c=1,carry=0;
-        for(int i=0;i<count;i++)
+        while(true)
         {
-           // System.out.println(s1);
+            long d=n%10;
+            n/=10;
+           // System.out.println(d);
             if(s1+carry>target)
             {
                 if(i==0)
                 {
-                    ans=10-arr[i];
+                    ans=10-d;
                     carry=1;
+                    i=1;
                 }
                 else
-                    ans=(9-arr[i])*(c)+ans;
-                s1-=arr[i];
-                c*=10;
-               // System.out.println("ans ->"+ans);
+                    ans=(9-d)*(c)+ans;
+                s1-=d;
+                c*=10; 
             }
             else
                 break;
